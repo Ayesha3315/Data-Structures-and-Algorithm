5ca411e4af366326d1a13bedccdcce15487bab5b
Ayesha Fatima <ayesha.fatima3315@gmail.com>
2026-08-31 23:37:10 +0530

    Corrected

--- Arrays/SortSquares.java
+++ Arrays/SortSquares.java
@@ -3,6 +3,7 @@ package Arrays;
 import java.util.*;
 
 public class SortSquares {
+
     public static void printArray(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -14,13 +15,13 @@ public class SortSquares {
         int left = 0;
         int right = arr.length - 1;
         int ans[] = new int[arr.length];
-        int k = 0;
-        while (left < right) {
+        int k = arr.length - 1;
+        while (left <= right) {
             if (Math.abs(arr[left]) > Math.abs(arr[right])) {
-                ans[k++] = arr[left] * arr[left];
+                ans[k--] = arr[left] * arr[left];
                 left++;
             } else {
-                ans[k++] = arr[right];
+                ans[k--] = arr[right] * arr[right];
                 right--;
             }
         }
@@ -29,17 +30,19 @@ public class SortSquares {
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter the size of the array:");
         int n = sc.nextInt();
+
         int arr[] = new int[n];
         System.out.println("Enter the Array elements:");
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
+
         int ans[] = sortSquares(arr);
         System.out.println("Sorted Squares:");
         printArray(ans);
         sc.close();
-
     }
 }

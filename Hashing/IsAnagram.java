4fc44b2bb5ae38dd233e48f6dbb59d05de019acb
Ayesha Fatima <ayesha.fatima3315@gmail.com>
2026-09-02 20:01:47 +0530

    Is Anagram

---
+++ Hashing/IsAnagram.java
@@ -0,0 +1,36 @@
+package Hashing;
+
+import java.util.*;
+
+public class IsAnagram {
+    public static HashMap<Character, Integer> makeFrequencyMap(String str) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            Character cr = str.charAt(i);
+            if (!map.containsKey(cr)) {
+                map.put(cr, 1);
+            } else {
+                map.put(cr, map.get(cr) + 1);
+            }
+        }
+        return map;
+    }
+
+    public static boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length())
+            return false;
+        HashMap<Character, Integer> map1 = makeFrequencyMap(s1);
+        HashMap<Character, Integer> map2 = makeFrequencyMap(s2);
+        return map1.equals(map2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first string:");
+        String s1 = sc.next();
+        System.out.println("Enter the second string:");
+        String s2 = sc.next();
+        System.out.println("Is the two strings Anagram : " + isAnagram(s1, s2));
+        sc.close();
+    }
+}

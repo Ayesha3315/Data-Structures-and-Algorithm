e5ac57a59d4d12b0622557a120553c509c1e7c61
Ayesha Fatima <ayesha.fatima3315@gmail.com>
2026-09-12 13:31:31 +0530

    Sort array by parity

---
+++ Leetcode/LC0027_RemoveElement/SortArrayByParity.java
@@ -0,0 +1,48 @@
+package Leetcode.LC0027_RemoveElement;
+
+import java.util.*;
+
+public class SortArrayByParity {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int[] sortArrayByParity(int arr[]) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            if (arr[left] % 2 != 0 && arr[right] % 2 == 0) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+            if (arr[left] % 2 == 0) {
+                left++;
+            }
+            if (arr[right] % 2 != 0) {
+                right--;
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the array elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int ans[] = sortArrayByParity(arr);
+        System.out.println("The array after sorting all even in the beginning and odd elements at the end: ");
+        printArray(ans);
+        sc.close();
+    }
+}
